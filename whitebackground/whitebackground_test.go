06843dc92760846827cd2e68c81b0155a2643313
Scott Ranger <millenniumnight@gogolemail.com>
2017-11-18 12:42:43 +0000

    Extra benchmarks for larger images

--- whitebackground/whitebackground_test.go
+++ whitebackground/whitebackground_test.go
@@ -9,7 +9,11 @@ import (
 	"testing"
 )
 
+// 800 x 800
 const url string = "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3871999.jpg"
+
+// 5627 × 3517
+const bigURL string = "https://www.walldevil.com/wallpapers/w04/137357-artwork-flowers-scars-style-white-background-women-women-artwork.jpg"
 
 func TestHasWhiteBackground(t *testing.T) {
 	runtime.GOMAXPROCS(4)
@@ -31,34 +35,49 @@ func TestHasWhiteBackground(t *testing.T) {
 	}
 }
 
-func BenchmarkHasWhiteBackground_1CPU(b *testing.B) {
-	benchmarkHasWhiteBackgroundCore(1, b)
+func BenchmarkHasWhiteBackground_1CPU_800x600(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(1, b, url)
 }
 
-func BenchmarkHasWhiteBackground_2CPU(b *testing.B) {
-	benchmarkHasWhiteBackgroundCore(2, b)
+func BenchmarkHasWhiteBackground_2CPU_800x600(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(2, b, url)
 }
 
-func BenchmarkHasWhiteBackground_3CPU(b *testing.B) {
-	benchmarkHasWhiteBackgroundCore(3, b)
+func BenchmarkHasWhiteBackground_3CPU_800x600(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(3, b, url)
 }
 
-func BenchmarkHasWhiteBackground_4CPU(b *testing.B) {
-	benchmarkHasWhiteBackgroundCore(4, b)
+func BenchmarkHasWhiteBackground_4CPU_800x600(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(4, b, url)
 }
 
-func benchmarkHasWhiteBackgroundCore(cpus int, b *testing.B) {
+func BenchmarkHasWhiteBackground_1CPU_5627x3517(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(1, b, bigURL)
+}
+
+func BenchmarkHasWhiteBackground_2CPU_5627x3517(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(2, b, bigURL)
+}
+
+func BenchmarkHasWhiteBackground_3CPU_5627x3517(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(3, b, bigURL)
+}
+
+func BenchmarkHasWhiteBackground_4CPU_5627x3517(b *testing.B) {
+	benchmarkHasWhiteBackgroundCore(4, b, bigURL)
+}
+
+func benchmarkHasWhiteBackgroundCore(cpus int, b *testing.B, url string) {
 	runtime.GOMAXPROCS(cpus)
 
-	// don't worry about errors
-	response, e := http.Get(url)
-	if e != nil {
-		log.Fatal(e)
+	r, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	defer response.Body.Close()
+	defer r.Body.Close()
 
-	i, _, err := image.Decode(response.Body)
+	i, _, err := image.Decode(r.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
